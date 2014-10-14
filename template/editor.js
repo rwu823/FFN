cf8964fff914f45e98d838a38e926fa38aaf85e0
Hiroshi Chu <chris.chu0903@gmail.com>
2014-10-14 16:19:06 +0800

    update fonts

--- template/editor.js
+++ template/editor.js
@@ -459,4 +459,16 @@ define(function(require, exports, module) {
 
     Main.init()
 
+    WebFontConfig = {
+        google: { families: [ 'Noto+Sans::latin' ] }
+    };
+    (function() {
+        var wf = document.createElement('script');
+        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+                  '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+        wf.type = 'text/javascript';
+        wf.async = 'true';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(wf, s);
+    })();
 })
